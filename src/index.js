73fdef9c414db441ef8e502c02a978f5ee38b1bb
Akash <veisthebest3@gmail.com>
2023-08-18 18:29:30 +0530

    Basic implementation of todolist done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import { format } from 'date-fns';
 import { projects, tasks, maketodotext, dom, newcollection, makecoll, makecolltext, maketodo, todologiccreate, maketododate } from './DOM.js';
 let project = [];
-
-dom();
-todologiccreate()
+let selectedcollection;
 
 //creates a collection
 function createcollection() {
@@ -13,11 +11,13 @@ function createcollection() {
             collectionname = makecolltext.value
             makecolltext.value = ""
         }
+
         const collection =
         {
             name: collectionname,
             todo: []
         }
+
         if (collectionname)
             project.push(collection);
         // this logic ensures that the past values of the array aren't being shown.
@@ -33,8 +33,12 @@ function createcollection() {
 //displays the collection
 function collectiondisplay() {
     project.forEach(function (collection) {
-        const coll = document.createElement("div")
+        let coll = document.createElement("div")
         coll.classList.add("Collections")
+        coll.addEventListener('click', function (event) {
+            selectedcollection = event.target.textContent
+            tododisplay(selectedcollection)
+        })
         coll.textContent = collection.name
         projects.appendChild(coll)
     })
@@ -47,10 +51,10 @@ function collectiondisplay() {
 
 
 //creates todo
-function createtodo() {
-    const collectionname = "Music";
+function createtodo(collectionname) {
+    console.log("hello")
     const title = maketodotext.value
-    maketodotext.textContent = ""
+    maketodotext.value = ""
     const preMadeCollection = project.find(collection => collection.name === collectionname)
     const due_date = format(new Date(maketododate.value), "yyyy-MM-dd")
     const values = {
@@ -58,43 +62,48 @@ function createtodo() {
         due_date: due_date,
     }
     preMadeCollection.todo.push(values);
-    tododisplay()
+    console.log(preMadeCollection.todo.length)
+    tododisplay(selectedcollection)
+
 }
 
 
 
 //to display all todo
-function tododisplay() {
-    let selectedcollection;
+function tododisplay(selectedcollection) {
+    while (tasks.firstChild.nextSibling) {
+        tasks.removeChild(tasks.firstChild.nextSibling);
+    }
+    const temp = selectedcollection
     let collections = document.querySelectorAll(".Collections")
-    console.log(collections)
-    collections.forEach(function (collection) {
-        collection.addEventListener("click", function (event) {
-            selectedcollection = event.target.textContent;
-            selectedcollection = project.find(function (collection) {
-                return collection.name === selectedcollection
-            })
-            console.log(selectedcollection)
-            selectedcollection.todo.forEach(function (values) {
-                const todoelement = document.createElement("div")
-                todoelement.textContent = values.title;
-                const tododate = document.createElement("div")
-                tododate.textContent = values.due_date
-                todoelement.appendChild(tododate)
-                tasks.appendChild(todoelement)
-                todologiccreate()
-
-            })
-
-
-        })
+    //event listeners are added, whenever they are clicked the following happens
+    selectedcollection = project.find(function (collection) {
+        return collection.name === selectedcollection
     })
+    console.log(selectedcollection)
+    selectedcollection.todo.forEach(function (values) {
+        const todoelement = document.createElement("div")
+        todoelement.textContent = values.title;
+        const tododate = document.createElement("div")
+        tododate.textContent = values.due_date
+        todoelement.appendChild(tododate)
+        tasks.appendChild(todoelement)
+    })
+    selectedcollection = temp
+    //input stuff
+    todologiccreate()
+
+
 
 
 }
 
 
 
+dom();
 makecoll.addEventListener('click', createcollection)
-maketodo.addEventListener('click', createtodo)
+maketodo.addEventListener('click', function (event) {
+    createtodo(selectedcollection)
+
+})
 collectiondisplay();
